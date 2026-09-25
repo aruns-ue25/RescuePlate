750dfc5f75e072ada862cd2ea073b647ffa2f364
Arun <sugarthan.arun@gmail.com>
2026-09-25 11:42:50 +0530

    feat: update request workflow database context

--- backend/RequestWorkflowService/Data/RequestDbContext.cs
+++ backend/RequestWorkflowService/Data/RequestDbContext.cs
@@ -18,11 +18,14 @@ public class RequestDbContext : DbContext
         modelBuilder.Entity<FoodRequest>(entity =>
         {
             entity.HasKey(e => e.Id);
+            entity.Property(e => e.DonationTitle).IsRequired().HasMaxLength(200);
             entity.Property(e => e.OrganizationId).IsRequired().HasMaxLength(100);
             entity.Property(e => e.OrganizationName).IsRequired().HasMaxLength(150);
             entity.Property(e => e.DonorId).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Unit).HasMaxLength(50);
             entity.Property(e => e.Status).IsRequired().HasMaxLength(50);
             entity.Property(e => e.Notes).HasMaxLength(500);
+            entity.Property(e => e.RejectionReason).HasMaxLength(500);
 
             entity.HasIndex(e => e.DonationId);
             entity.HasIndex(e => e.OrganizationId);
